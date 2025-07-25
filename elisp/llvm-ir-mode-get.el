2accd1e91157ec9988037ef2ac86bb86a6618398
Afonso Rafael <afonsorafael@sapo.pt>
2025-07-25 15:19:45 +0200

    Change[llvm-modes]: Stop generating the folder on the wrong path...
    Was not creating the folder and files inside the '.emacs.d folder
    
    Was not creating the folder and files inside the ".emacs.d" folder

--- elisp/llvm-ir-mode-get.el
+++ elisp/llvm-ir-mode-get.el
@@ -1,21 +1,21 @@
 (require 'url)
 (defun llvm-mode-get ()
   (interactive)
-  (when (not (file-directory-p "llvm-mode"))
-    (make-directory "llvm-mode")
+  (when (not (file-directory-p "~/.emacs.d/llvm-mode"))
+    (make-directory "~/.emacs.d/llvm-mode")
     (url-copy-file
      (concat "https://raw.githubusercontent.com/llvm/llvm-project/"
 	     "refs/heads/main/llvm/utils/emacs/emacs.el")
-     "llvm-mode/emacs.el")
+     "~/.emacs.d/llvm-mode/emacs.el")
     (url-copy-file
      (concat "https://raw.githubusercontent.com/llvm/llvm-project/"
 	     "refs/heads/main/llvm/utils/emacs/llvm-mir-mode.el")
-     "llvm-mode/llvm-mir-mode.el")
+     "~/.emacs.d/llvm-mode/llvm-mir-mode.el")
     (url-copy-file
      (concat "https://raw.githubusercontent.com/llvm/llvm-project/"
 	     "refs/heads/main/llvm/utils/emacs/llvm-mode.el")
-     "llvm-mode/llvm-mode.el")
+     ".emacs.d/llvm-mode/llvm-mode.el")
     (url-copy-file
      (concat "https://raw.githubusercontent.com/llvm/llvm-project/"
 	     "refs/heads/main/llvm/utils/emacs/tablegen-mode.el")
-     "llvm-mode/tablegen-mode.el")))
+     ".emacs.d/llvm-mode/tablegen-mode.el")))
